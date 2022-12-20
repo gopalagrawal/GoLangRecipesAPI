1eabe8b76a5ceb9454bbcda857de91f25135132e
gopalagrawal <ga.willbe@gmail.com>
2022-12-20 22:52:14 +0530

    Post: /recipes

--- main.go
+++ main.go
@@ -2,26 +2,51 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 
 type Recipe struct {
+	ID 				string 		`json:"id"`
 	Name 			string 		`json:"name"`
 	Tags 			[]string 	`json:"tags"`
 	Ingredients 	[]string 	`json:"ingredients"`
 	Instructions 	[]string 	`json:"instructions"`
 	PublishedAt 	time.Time 	`json:"publishedAt"`
+}
+
+var recipes []Recipe
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
+// POST: /recipes
+// ID and PublishedAt are auto-generated.
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil { 
+		c.JSON(http.StatusBadRequest, gin.H{ "error": err.Error() })
+		return
 	}
-
-
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
 
 
 
 func main() {
 	fmt.Println("Starting Server on localhost:8080 ... ")
 	router := gin.Default()
+
+	// Create routes
+	router.POST("/recipes", NewRecipeHandler)
+
+
 	router.Run(":8080")
 }
